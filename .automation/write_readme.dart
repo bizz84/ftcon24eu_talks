82dc41a41e991feace7b5df170b1237509b21cc8
Martin Bertele <dev+git@martin-bertele.com>
2024-07-15 07:45:35 +0200

    improved README intro

--- .automation/write_readme.dart
+++ .automation/write_readme.dart
@@ -8,18 +8,31 @@ void writeReadme(List<Talk> talks) async {
   readmeContent.writeln('# FlutterCon Europe 2024\n\n');
 
   readmeContent.writeln(
-      'In Progress: Happy to receive your PR for missing information and fixing things :) \n');
+      'I prepared jsons and scripts for having different views of the Fluttercon 2024 content. [Kilian Schulte](https://github.com/schultek) provided the [FlutterCon data](https://fluttercon.schultek.de/), which I fed into ChatGPT to retrieve json files.\n');
+
+  readmeContent.writeln('## Acknowledgement\n');
+  readmeContent.writeln(
+      '[Andrea Bizzotto\'s](https://github.com/bizz84) inspired me with his repo [FlutterCon 2023 resources](https://github.com/bizz84/fluttercon_23_resources).\n');
+
+  readmeContent.writeln('## Contribution\n');
+  readmeContent.writeln(
+      'You\'re very welcome to fill any info inside the [talks/](https://github.com/martin-bertele/ftcon24eu/tree/main/talks). The file names there should be the talks titles. If you enjoy any other view of the data, you could look at the [scripts](https://github.com/martin-bertele/ftcon24eu/tree/main/.automation) as a starting point. Happy to recieve your PR :)\n');
+
+  readmeContent.writeln('# Talks\n\n');
+
+  readmeContent.writeln('Here you can find more views created so far:\n');
 
   readmeContent.writeln(
       '[Agenda](https://github.com/martin-bertele/ftcon24eu/blob/main/Speakers.md)\n');
-  readmeContent.writeln(
-      '[List of speakers](https://github.com/martin-bertele/ftcon24eu/blob/main/Speakers.md)\n');
-  readmeContent.writeln(
-      '[Talks sorted by rooms](https://github.com/martin-bertele/ftcon24eu/blob/main/Rooms.md)\n');
-  readmeContent.writeln(
-      '[Talks sorted by formats](https://github.com/martin-bertele/ftcon24eu/blob/main/Formats.md)\n');
 
-  readmeContent.writeln('# Talks\n\n');
+  readmeContent.writeln(
+      'Speakers [list](https://github.com/martin-bertele/ftcon24eu/blob/main/Speakers.md) and all [socials](https://github.com/martin-bertele/ftcon24eu/blob/main/Socials.md)\n');
+
+  readmeContent.writeln(
+      'Talks sorted by: [rooms](https://github.com/martin-bertele/ftcon24eu/blob/main/Rooms.md) | [formats](https://github.com/martin-bertele/ftcon24eu/blob/main/Formats.md) | [lebels (in progress)](https://github.com/martin-bertele/ftcon24eu/blob/main/Levels.md)\n');
+
+  readmeContent.writeln(
+      '[Talks with full descriptions](https://github.com/martin-bertele/ftcon24eu/blob/main/Descriptions.md)\n');
 
   // Generate table headers
   readmeContent.writeln('| Title | Speakers | Resources | Recommendations |');
